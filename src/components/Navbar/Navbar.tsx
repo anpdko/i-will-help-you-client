a3a57cfdeaa4f767936fff296da2e46790327fd7
Olesia Andreiko <olesikmail@gmail.com>
2023-08-18 22:10:19 +0300

    Create navBar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,44 @@
 import s from './Navbar.module.scss'
+import { NavLink } from "react-router-dom";
 import { Translation } from '../../components'
+import { useTranslation } from "react-i18next";
 
 // use <div class='container'></div>
 const Navbar = () => {
+   const { t } = useTranslation();
+
+   const links = [
+     {
+       text: "Ready to Help",
+       link: "/readyneed",
+     },
+     {
+       text: "Need Help",
+       link: "/needhelp",
+     },
+     {
+       text: "Projects",
+       link: "/projects",
+     },
+     {
+       text: "About us",
+       link: "/about",
+     },
+   ];
+
    return (
       <nav className={s.nav}>
-         <h1>Navbar</h1>
+         <ul className={s.nav__list}>
+        {links
+          ? links.map((link, i) => (
+              <li key={i} className={s.nav__item}>
+                <NavLink to={link.link} className={s.nav__link}>
+                  {t(link.text)}
+                </NavLink>
+              </li>
+            ))
+          : null}
+      </ul>
          <Translation/>
       </nav>
    );
