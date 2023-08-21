2ea5e314626036344805fed411d68f5e99227087
Alina Shtykh <alinashtykh@gmail.com>
2023-08-21 22:51:27 +0200

    project component style

--- src/components/Projects/Project/Project.tsx
+++ src/components/Projects/Project/Project.tsx
@@ -9,11 +9,15 @@ interface ProjectProps {
 const Project = ({ project }: ProjectProps) => {
 	return (
 		<div className={s.project}>
-			<h2>{project.title}</h2>
-			<p>{project.description}</p>
-			<Link to={project.link}></Link>
+			<div className={s.project__content}>
+				<h2 className={`${s.project__title} heading2`}>{project.title}</h2>
+				<p className={s.project__description}>{project.description}</p>
+				<Link className={s.project__link} to={project.link}></Link>
+			</div>
 
-			<img src={project.img} alt="project image" />
+			<div className={s.project__img}>
+				<img src={project.img} alt="project image" />
+			</div>
 		</div>
 	);
 };
