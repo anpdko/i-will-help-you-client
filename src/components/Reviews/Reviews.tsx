a5de7b65237efa02b070b28a7d3921c730b5fb48
Alina Shtykh <alinashtykh@gmail.com>
2023-08-23 01:33:01 +0200

    created review data

--- src/components/Reviews/Reviews.tsx
+++ src/components/Reviews/Reviews.tsx
@@ -1,10 +1,37 @@
-import s from './Reviews.module.scss'
+import s from './Reviews.module.scss';
 
 const Reviews = () => {
-   return (
-      <div className={s.reviews}>
-         
-      </div>
-   );
+	return <div className={s.reviews}></div>;
 };
-export default Reviews
+export default Reviews;
+// // import { Swiper, SwiperSlide } from 'swiper/react'
+// import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+// // Import Swiper styles
+// import 'swiper/scss';
+// import 'swiper/scss/navigation';
+// import 'swiper/scss/pagination';
+// import 'swiper/scss/scrollbar';
+//
+// interface SwiperSliderProps {
+// 	images: string[];
+// }
+//
+// export const SwiperSlider: React.FC<SwiperSliderProps> = () => {
+// 	return (
+// 		<Swiper
+// 			modules={[Navigation, Pagination, Scrollbar, A11y]}
+// 			spaceBetween={50}
+// 			slidesPerView={3}
+// 			navigation
+// 			pagination={{ clickable: true }}
+// 			scrollbar={{ draggable: true }}
+// 			onSwiper={(swiper) => console.log(swiper)}
+// 			onSlideChange={() => console.log('slide change')}
+// 		>
+// 			<SwiperSlide>Slide 1</SwiperSlide>
+// 			<SwiperSlide>Slide 2</SwiperSlide>
+// 			<SwiperSlide>Slide 3</SwiperSlide>
+// 			<SwiperSlide>Slide 4</SwiperSlide>
+// 		</Swiper>
+// 	);
+// };
