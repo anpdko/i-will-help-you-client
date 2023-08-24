118e8c71f0444ef6e287bd901678374d38f45373
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-08-24 14:15:50 +0300

    feat: add new rules for eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,46 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: './tsconfig.json',
+  },
+  plugins: [
+    'react-refresh',
+    'import',
+    'prettier',
+    'react',
+    'react-hooks',
+    '@typescript-eslint',
+  ],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true,
+        parser: 'flow',
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'jsx-a11y/img-redundant-alt': 'off',
+    'jsx-a11y/click-events-have-key-events': 'warn',
+    'jsx-a11y/no-static-element-interactions': 'warn',
   },
-}
+};
