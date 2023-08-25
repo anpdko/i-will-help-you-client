fd0bb7218a483a04fa81eb59920f3591191451e2
Andreii Pryadko <50361705+anpdko@users.noreply.github.com>
2023-08-25 21:31:55 +0300

    Update ProjectHeader.tsx

--- src/components/ProjectHeader/ProjectHeader.tsx
+++ src/components/ProjectHeader/ProjectHeader.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { Navigation, Controller, EffectFade } from 'swiper/modules';
