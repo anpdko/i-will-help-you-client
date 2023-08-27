7d706cdfda72e2da4c6cca2f4c59dc5066899fba
DmytroKuts <40741910+Dmytro24@users.noreply.github.com>
2023-08-28 00:37:09 +0300

    Made an adaptation for Volunteer

--- src/store/reviews/reviewsSlice.ts
+++ src/store/reviews/reviewsSlice.ts
@@ -1,71 +1,72 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import axios, {AxiosResponse, AxiosError} from "axios";
-import { IReviews, ReviewsState, IReviewsError} from './reviewsType'
+import axios, { AxiosResponse, AxiosError } from 'axios';
+import { IReviews, ReviewsState, IReviewsError } from './reviewsType';
 // import authHeader from '../../services/admin/header.service'
 
-const API_URL = import.meta.env.VITE_API_URL 
+const API_URL = import.meta.env.VITE_API_URL;
 
 const initialState: ReviewsState = {
-   reviews: [],
-   loading: null,
-   message: null,
+  reviews: [],
+  loading: null,
+  message: null,
 };
 
 const handleRequestError = (error: AxiosError<IReviewsError>) => {
-   if(window.location.hostname === 'localhost'){
-      console.log(error)
-   }
-   if (axios.isAxiosError(error) && error.response) {
-      if (error.response?.status === 401) {
-         alert(error.response.data.message);
-         // logout with admin
-      }
-      if (error.response.data.message){
-         return error.response.data.message;
-      }
-      if (error.message){
-         return error.message;
-      }
-   }
-   return 'Server error';
+  if (window.location.hostname === 'localhost') {
+    console.log(error);
+  }
+  if (axios.isAxiosError(error) && error.response) {
+    if (error.response?.status === 401) {
+      alert(error.response.data.message);
+      // logout with admin
+    }
+    if (error.response.data.message) {
+      return error.response.data.message;
+    }
+    if (error.message) {
+      return error.message;
+    }
+  }
+  return 'Server error';
 };
 
 export const getReviews = createAsyncThunk(
-   'reviews/getReviews',
-   async (_, { fulfillWithValue, rejectWithValue }) => {
-      try {
-         const res: AxiosResponse<IReviews[]> = await axios.get(`${API_URL}/api/reviews`);
-         return fulfillWithValue(res.data);
-      } catch (error) {
-         if (axios.isAxiosError(error)){
-            return rejectWithValue(handleRequestError(error))
-         }
+  'reviews/getReviews',
+  async (_, { fulfillWithValue, rejectWithValue }) => {
+    try {
+      const res: AxiosResponse<IReviews[]> = await axios.get(
+        `${API_URL}/api/reviews`,
+      );
+      return fulfillWithValue(res.data);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        return rejectWithValue(handleRequestError(error));
       }
-   }
+    }
+  },
 );
 
 export const reviewsSlice = createSlice({
-   name: 'reviews',
-   initialState,
-   reducers: {},
-   extraReducers: (builder) => {
-      builder
-         // getReviews
-         .addCase(getReviews.pending, (state) => {
-            state.loading = true;
-            state.message = null;
-         })
-         .addCase(getReviews.fulfilled, (state, action) => {
-            state.reviews = action.payload || [];
-            state.loading = false;
-         })
-         .addCase(getReviews.rejected, (state, action) => {
-            state.loading = false;
-            state.message = String(action.payload);
-         })
-   }
-})
+  name: 'reviews',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      // getReviews
+      .addCase(getReviews.pending, (state) => {
+        state.loading = true;
+        state.message = null;
+      })
+      .addCase(getReviews.fulfilled, (state, action) => {
+        state.reviews = action.payload || [];
+        state.loading = false;
+      })
+      .addCase(getReviews.rejected, (state, action) => {
+        state.loading = false;
+        state.message = String(action.payload);
+      });
+  },
+});
 
-
-export const {} = reviewsSlice.actions
-export default reviewsSlice.reducer
+export const {} = reviewsSlice.actions;
+export default reviewsSlice.reducer;
