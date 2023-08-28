689d2a8e92a5f7da1b0180ea4db18dd9666b603c
DmytroKuts <40741910+Dmytro24@users.noreply.github.com>
2023-08-28 16:22:25 +0300

    fixed navbar, created burger menu and add follow us and contact block. created asset icons .svg

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -55,25 +55,73 @@ const Navbar = () => {
             </svg>
           </Link>
         </div>
-        <div className={s.nav__menu}>
-          <nav className={s.nav__body}>
-            <ul className={s.nav__list}>
-              {links
-                ? links.map((link, i) => (
-                    <li key={i} className={s.nav__item}>
-                      <NavLink
-                        to={link.link}
-                        className={s.nav__link}
-                        onClick={handleMenuLinkClick}
-                      >
-                        {t(link.text)}
-                      </NavLink>
-                    </li>
-                  ))
-                : null}
+        <nav className={s.nav__menu}>
+          <ul className={s.nav__list}>
+            {links
+              ? links.map((link, i) => (
+                  <li key={i} className={s.nav__item}>
+                    <NavLink
+                      to={link.link}
+                      className={s.nav__link}
+                      onClick={handleMenuLinkClick}
+                    >
+                      {t(link.text)}
+                      <svg>
+                        <use href={sprite + '#arrow-menu'}></use>
+                      </svg>
+                    </NavLink>
+                  </li>
+                ))
+              : null}
+          </ul>
+          <div className={s.social}>
+            <h3 className={s.social__title}>{t('Follow us')}</h3>
+            <ul className={s.social__list}>
+              <li>
+                <Link to='#' className={s.social__link}>
+                  <svg>
+                    <use href={sprite + '#facebook'} />
+                  </svg>
+                </Link>
+              </li>
+              <li>
+                <Link to='#' className={s.social__link}>
+                  <svg>
+                    <use href={sprite + '#instagram'} />
+                  </svg>
+                </Link>
+              </li>
             </ul>
-          </nav>
-        </div>
+          </div>
+
+          <ul className={s.contacts}>
+            <li className={s.contacts__item}>
+              <Link to='tel:++380000000000' className={s.contacts__link}>
+                <div className={s.contacts__link_text}>
+                  <span>Hotline</span>
+                  +0 (000) 000 00 00
+                </div>
+                <svg>
+                  <use href={sprite + '#phone'} />
+                </svg>
+              </Link>
+            </li>
+            <li className={s.contacts__item}>
+              <Link
+                to='mailto:iwillhelpu@post.com'
+                className={s.contacts__link}
+              >
+                <div className={s.contacts__link_text}>
+                  <span>Email</span>
+                  iwillhelpu@post.com
+                </div>
+                <svg>
+                  <use href={sprite + '#mail'} />
+                </svg>
+              </Link>
+            </li>
+          </ul>
+        </nav>
         <Translation />
         <ButtonApp>{t('Donate')}</ButtonApp>
         <button
