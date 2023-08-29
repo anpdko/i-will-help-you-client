1fe543e1d83810e2a36a51cf3c64514e05f987a8
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-08-29 11:17:42 +0300

    refactor: apply prettier

--- src/components/Projects/Project/Project.tsx
+++ src/components/Projects/Project/Project.tsx
@@ -2,8 +2,8 @@ import { Link } from 'react-router-dom';
 import { subText } from '../../../services/text.services';
 import sprite from '../../../assets/sprite.svg';
 import s from './Project.module.scss';
-import {useTranslation} from 'react-i18next'
-import {IProject} from '../../../store/projects/projectsType'
+import { useTranslation } from 'react-i18next';
+import { IProject } from '../../../store/projects/projectsType';
 
 interface ProjectProps {
   project: IProject;
@@ -12,23 +12,25 @@ interface ProjectProps {
 const API_URL = import.meta.env.VITE_API_URL;
 
 const Project = ({ project }: ProjectProps) => {
-  const {i18n} = useTranslation()
+  const { i18n } = useTranslation();
 
   const transProject = () => {
-    return project.translations.find((item) => i18n.language === item.language)
-  }
+    return project.translations.find((item) => i18n.language === item.language);
+  };
 
   return (
     <article className={s.project}>
       <div className={s.project__content}>
         <div>
-          <h2 className={`${s.project__title} heading2`}>{transProject()?.title}</h2>
+          <h2 className={`${s.project__title} heading2`}>
+            {transProject()?.title}
+          </h2>
           <p className={`${s.project__description} text`}>
             {subText(String(transProject()?.description), 800)}
           </p>
         </div>
         <div>
-          <Link className={s.project__btn} to={"/"}>
+          <Link className={s.project__btn} to={'/'}>
             <svg className={s.project__icon}>
               <use href={sprite + '#arrow-up-right'} />
             </svg>
@@ -37,7 +39,10 @@ const Project = ({ project }: ProjectProps) => {
       </div>
 
       <div className={s.project__img}>
-        <img src={API_URL + '/static/images/projects/' + project.imgCover} alt='project image' />
+        <img
+          src={API_URL + '/static/images/projects/' + project.imgCover}
+          alt='project image'
+        />
       </div>
     </article>
   );
