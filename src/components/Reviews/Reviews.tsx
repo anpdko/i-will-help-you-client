c09fb9a735aca57697d139f059be5831355c9c49
anpdko <anpdko@gmail.com>
2023-08-29 10:26:10 +0300

    fix AuthAdminPage

--- src/components/Reviews/Reviews.tsx
+++ src/components/Reviews/Reviews.tsx
@@ -82,6 +82,7 @@ const Reviews = () => {
   return (
     <section>
       <div className='container'>
+        <h2 className='heading1'>Stories of Hope and Gratitude</h2>
         <div className={s.reviews}>
           <div
             className={s.reviews__tabs}
