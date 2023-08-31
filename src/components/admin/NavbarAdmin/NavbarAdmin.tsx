b06339bc13e3aee3238c829258e388858f52d8f1
DmytroKuts <40741910+Dmytro24@users.noreply.github.com>
2023-08-31 11:07:42 +0300

    create active link color + run format

--- src/components/admin/NavbarAdmin/NavbarAdmin.tsx
+++ src/components/admin/NavbarAdmin/NavbarAdmin.tsx
@@ -1,7 +1,7 @@
 import s from './NavbarAdmin.module.scss';
-import {AppDispatch} from '../../../store/store'
-import { adminLogout } from '../../../store/admin/adminSlice'
-import { useDispatch } from 'react-redux'
+import { AppDispatch } from '../../../store/store';
+import { adminLogout } from '../../../store/admin/adminSlice';
+import { useDispatch } from 'react-redux';
 
 const NavbarAdmin = () => {
   const dispatch = useDispatch<AppDispatch>();
@@ -9,9 +9,9 @@ const NavbarAdmin = () => {
     <div className={s.navbar_admin}>
       <h1>NavbarAdmin</h1>
       <ul>
-        <li onClick = {() => dispatch(adminLogout())}>
-            <i className="bi bi-box-arrow-left"></i>
-            <span>Выйти</span>
+        <li onClick={() => dispatch(adminLogout())}>
+          <i className='bi bi-box-arrow-left'></i>
+          <span>Выйти</span>
         </li>
       </ul>
     </div>
