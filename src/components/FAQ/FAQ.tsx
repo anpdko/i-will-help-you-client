803603a01ee5fb6c9ad2c4444aa15ac87754f385
Anastasia Koptilova <koptilova.nastya@gmail.com>
2023-09-01 16:28:40 +0300

    add heading styles

--- src/components/FAQ/FAQ.tsx
+++ src/components/FAQ/FAQ.tsx
@@ -1,11 +1,11 @@
 import s from './FAQ.module.scss';
 import faqData from '../../data/faqData';
-import React from 'react';
 import FAQItem from './FAQItem/FAQItem';
 
 const FAQ = () => {
   return (
     <section className={s.faq}>
+      <h2 className={`${s.faq_heading} heading2`}>FAQ</h2>
       <ul className={s.faq_list}>
         {faqData.map((el) => (
           <FAQItem key={el.id} tab={el} />
