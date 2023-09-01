7f9c6a2b43dd323ce9a624194039c6b75878197e
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-09-01 22:54:06 +0300

    feat: add select country to form

---
+++ src/components/FormReadyHelp/SelectCountry/SelectCountry.tsx
@@ -0,0 +1,40 @@
+import { Controller, useFormContext } from 'react-hook-form';
+import Select from 'react-select';
+import { countryList } from '../../../utils/listOfCountry';
+import FormItemContainer from '../FormItemContainer';
+import customStyles from '../selectStyle';
+import basic from '../FormReadyHelp.module.scss';
+import s from './SelectCountry.module.scss';
+
+const SelectCountry = () => {
+  const { control } = useFormContext();
+
+  const countries = countryList.map((country) => ({
+    value: country.name,
+    label: country.name,
+  }));
+
+  return (
+    <FormItemContainer section='country'>
+      <label htmlFor='country' className={basic.form__label}>
+        Country of Residence *
+      </label>
+      <Controller
+        name='country'
+        control={control}
+        render={({ field: { onChange, value } }) => (
+          <Select
+            options={countries}
+            value={countries.find((country) => country.value === value)}
+            onChange={(country) => onChange(country?.value)}
+            styles={customStyles}
+            placeholder='Select Country'
+          />
+        )}
+        rules={{ required: true }}
+      />
+    </FormItemContainer>
+  );
+};
+
+export default SelectCountry;
