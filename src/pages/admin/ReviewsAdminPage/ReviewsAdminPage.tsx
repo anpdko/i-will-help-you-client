c58cdab0f3de629b1cf3bf06fe0a8ecdd208b545
DmytroKuts <40741910+Dmytro24@users.noreply.github.com>
2023-09-02 02:47:21 +0300

    merge and format

--- src/pages/admin/ReviewsAdminPage/ReviewsAdminPage.tsx
+++ src/pages/admin/ReviewsAdminPage/ReviewsAdminPage.tsx
@@ -3,12 +3,10 @@ import TableReviewsAdmin from './TableReviewsAdmin';
 import { NavbarControlAdmin } from '../../../components/admin';
 
 const ReviewsAdminPage = () => {
-
   return (
     <div className={s.reviews_admin_page}>
-
-      <NavbarControlAdmin/>
-      <TableReviewsAdmin/>
+      <NavbarControlAdmin />
+      <TableReviewsAdmin />
     </div>
   );
 };
