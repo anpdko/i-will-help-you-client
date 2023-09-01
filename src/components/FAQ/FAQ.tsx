af828cb0033e03272f09b7609375ede808e225cf
Anastasia Koptilova <koptilova.nastya@gmail.com>
2023-09-01 17:03:00 +0300

    add flex

--- src/components/FAQ/FAQ.tsx
+++ src/components/FAQ/FAQ.tsx
@@ -5,12 +5,15 @@ import FAQItem from './FAQItem/FAQItem';
 const FAQ = () => {
   return (
     <section className={s.faq}>
-      <h2 className={`${s.faq_heading} heading2`}>FAQ</h2>
-      <ul className={s.faq_list}>
-        {faqData.map((el) => (
-          <FAQItem key={el.id} tab={el} />
-        ))}
-      </ul>
+      <h3 className={s.faq_subtitle}>FAQ</h3>
+      <div className={s.faq_container}>
+        <h2 className={`${s.faq_heading} heading2`}>FAQ</h2>
+        <ul className={s.faq_list}>
+          {faqData.map((el) => (
+            <FAQItem key={el.id} tab={el} />
+          ))}
+        </ul>
+      </div>
     </section>
   );
 };
