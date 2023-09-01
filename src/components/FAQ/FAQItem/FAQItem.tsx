ed6608c221697bc4ed202f0aaf1a3ddc721e00ce
Anastasia Koptilova <koptilova.nastya@gmail.com>
2023-09-01 20:01:11 +0300

    add btn

--- src/components/FAQ/FAQItem/FAQItem.tsx
+++ src/components/FAQ/FAQItem/FAQItem.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import s from './FAQItem.module.scss';
 import { IFaqData } from '../../../data/faqData';
+import sprite from '../../../assets/sprite.svg';
 
 interface FAQProps {
   tab: IFaqData;
@@ -15,7 +16,14 @@ const FAQItem = ({ tab }: FAQProps) => {
 
   return (
     <li key={tab.id} className={s.faq_item} onClick={handleToggleFAQ}>
-      <h4 className={s.faq_item__title}>{tab.title}</h4>
+      <div className={s.faq_item__container}>
+        <h4 className={s.faq_item__title}>{tab.title}</h4>
+        <button type='button' className={s.faq_item__btn}>
+          <svg className={toggleFAQ ? `${s.faq_item__icon_open}` : `${s.faq_item__icon_closed}`}>
+            <use href={sprite + '#chevron-down'} />
+          </svg>
+        </button>
+      </div>
       {toggleFAQ && <p className={s.faq_item__descr}>{tab.description}</p>}
     </li>
   );
