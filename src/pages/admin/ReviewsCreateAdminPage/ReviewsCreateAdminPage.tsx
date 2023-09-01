9c671a9f7ed451a4d40b537fbd45c0d33995ef13
anpdko <anpdko@gmail.com>
2023-09-01 14:02:20 +0300

    files

---
+++ src/pages/admin/ReviewsCreateAdminPage/ReviewsCreateAdminPage.tsx
@@ -0,0 +1,10 @@
+import s from './ReviewsCreateAdminPage.module.scss'
+
+const ReviewsCreateAdminPage = () => {
+   return (
+      <div className={s.reviews_create_admin_page}>
+         <h1>ReviewsCreateAdminPage</h1>
+      </div>
+   );
+};
+export default ReviewsCreateAdminPage
--- src/pages/admin/index.ts
+++ src/pages/admin/index.ts
@@ -1,5 +1,13 @@
 import AuthAdminPage from './AuthAdminPage/AuthAdminPage';
 import ReviewsAdminPage from './ReviewsAdminPage/ReviewsAdminPage';
+import ReviewsCreateAdminPage from './ReviewsCreateAdminPage/ReviewsCreateAdminPage';
+import ReviewsEditAdminPage from './ReviewsCreateAdminPage/ReviewsCreateAdminPage';
 import ProjectsAdminPage from './ProjectsAdminPage/ProjectsAdminPage';
 
-export { AuthAdminPage, ReviewsAdminPage, ProjectsAdminPage };
+export { 
+   AuthAdminPage, 
+   ReviewsAdminPage, 
+   ProjectsAdminPage,
+   ReviewsCreateAdminPage,
+   ReviewsEditAdminPage
+};
