1a127cb86bb83d320f3c1abd45eed5f23775f9bd
anpdko <anpdko@gmail.com>
2023-09-03 00:26:14 +0300

    admin_container

--- src/routes/RoutesAdmin.tsx
+++ src/routes/RoutesAdmin.tsx
@@ -25,30 +25,28 @@ const RoutesAdmin = () => {
   }, [dispatch, isLoggedIn]);
 
   return (
-    <main>
-      <div className='admin_container'>
-        {isLoggedIn && <NavbarAdmin />}
-        {isLoggedIn ? (
-          <Routes>
-            <Route path='/panel/reviews' element={<ReviewsAdminPage />} />
-            <Route path='/panel/review/create' element={<ReviewsCreateAdminPage />}/>
-            <Route path='/panel/review/edit/:id' element={<ReviewsEditAdminPage />}/>
+    <main className='admin_container'>
+      {isLoggedIn && <NavbarAdmin />}
+      {isLoggedIn ? (
+        <Routes>
+          <Route path='/panel/reviews' element={<ReviewsAdminPage />} />
+          <Route path='/panel/review/create' element={<ReviewsCreateAdminPage />}/>
+          <Route path='/panel/review/edit/:id' element={<ReviewsEditAdminPage />}/>
 
-            <Route path='/panel/projects' element={<ProjectsAdminPage />} />
-            <Route path='/panel/project/create' element={<ProjectsCreateAdminPage />} />
-            <Route path='/panel/project/edit/:id' element={<ProjectsEditAdminPage />} />
-            <Route
-              path='*'
-              element={<Navigate to={'/admin/panel/projects'} />}
-            />
-          </Routes>
-        ) : (
-          <Routes>
-            <Route path='/auth' element={<AuthAdminPage />} />
-            <Route path='*' element={<Navigate to={'/admin/auth'} />} />
-          </Routes>
-        )}
-      </div>
+          <Route path='/panel/projects' element={<ProjectsAdminPage />} />
+          <Route path='/panel/project/create' element={<ProjectsCreateAdminPage />} />
+          <Route path='/panel/project/edit/:id' element={<ProjectsEditAdminPage />} />
+          <Route
+            path='*'
+            element={<Navigate to={'/admin/panel/projects'} />}
+          />
+        </Routes>
+      ) : (
+        <Routes>
+          <Route path='/auth' element={<AuthAdminPage />} />
+          <Route path='*' element={<Navigate to={'/admin/auth'} />} />
+        </Routes>
+      )}
     </main>
   );
 };
