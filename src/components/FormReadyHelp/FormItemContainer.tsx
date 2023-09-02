8caaec389098519f03c11691ed9fea94a5777ce3
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-09-02 23:16:11 +0300

    fix: add className

--- src/components/FormReadyHelp/FormItemContainer.tsx
+++ src/components/FormReadyHelp/FormItemContainer.tsx
@@ -3,11 +3,22 @@ import basic from './FormReadyHelp.module.scss';
 interface FormItemContainer {
   children: React.ReactNode;
   section: string;
+  className?: string;
 }
 
-const FormItemContainer = ({ children, section }: FormItemContainer) => {
+const FormItemContainer = ({
+  children,
+  section,
+  className,
+}: FormItemContainer) => {
   return (
-    <div className={`${basic.form__wrap} ${basic[`form__${section}`]}`}>
+    <div
+      className={`
+        ${basic.form__wrap}
+        ${basic[`form__${section}`]}
+        ${className}
+      `}
+    >
       {children}
     </div>
   );
