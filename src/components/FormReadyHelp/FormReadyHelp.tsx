81b6a7612caf8585737769caa9738a892b567436
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-09-03 15:41:59 +0300

    fix: update form

--- src/components/FormReadyHelp/FormReadyHelp.tsx
+++ src/components/FormReadyHelp/FormReadyHelp.tsx
@@ -1,4 +1,4 @@
-import { useForm, FormProvider } from 'react-hook-form';
+import { useForm, FormProvider, SubmitHandler, FieldValues } from 'react-hook-form';
 import FullName from './FullName/FullName';
 import DateOfBirth from './DateOfBirth/DateOfBirth';
 import PhoneNumber from './PhoneNumber/PhoneNumber';
@@ -9,43 +9,97 @@ import DaysOfVolunteering from './DaysOfVolunteering/DaysOfVolunteering';
 import Language from './Language/Language';
 import Skills from './Skills/Skills';
 import Comment from './Comment/Comment';
-import Checkboxes from './Checkboxs/Checkboxes';
+import Checkboxes from './Checkboxes/Checkboxes';
 import s from './FormReadyHelp.module.scss';
 import { ButtonApp } from '../UI';
+import { convertUnixTimestampToDate } from '../../utils/convertUnixTimestampToDate';
+import { generateSocialMediaLink } from '../../utils/generateSocialMediaLink';
+
+interface DataForm {
+  firstName: string;
+  lastName: string;
+  dataOfBirth: number;
+  countryCode: string;
+  phone: string;
+  country: string;
+  network: string;
+  networkLogo: string;
+  email: string;
+  daysVolunteer: string[];
+  languages: string[];
+  skills: string[];
+  comment: string;
+  mailing: boolean;
+  dataProcessing: boolean;
+}
 
 const FormReadyHelp = () => {
   const methods = useForm({
     mode: 'onChange',
   });
 
-  const onSubmit = (data: any) => {
-    console.log(data);
+  const {
+    formState: { isValid },
+  } = methods;
+
+  const onSubmit = (data: DataForm) => {
+    const formattedData = {
+      firstName: data.firstName,
+      lastName: data.lastName,
+      dataOfBirth: convertUnixTimestampToDate(data.dataOfBirth),
+      phone: data.countryCode + data.phone,
+      country: data.country,
+      network: generateSocialMediaLink(data.network, data.networkLogo),
+      email: data.email,
+      daysVolunteer: data.daysVolunteer,
+      languages: data.languages,
+      skills: data.skills,
+      comment: data.comment,
+      mailing: data.mailing,
+    };
+
+    console.log(formattedData);
   };
 
   return (
-    <FormProvider {...methods}>
-      <form
-        id='form'
-        onSubmit={methods.handleSubmit(onSubmit)}
-        className={s.form}
-      >
-        <FullName nameType='firstName' />
-        <FullName nameType='lastName' />
-        <DateOfBirth />
-        <PhoneNumber />
-        <SelectCountry />
-        <SocialNetwork />
-        <Email />
-        <DaysOfVolunteering />
-        <Language />
-        <Skills />
-        <Comment />
-        <Checkboxes />
-        <ButtonApp type='submit' className={s.form__button}>
-          Submit
-        </ButtonApp>
-      </form>
-    </FormProvider>
+    <section className={s.section}>
+      <h3 className={s.section__subtitle}>Form</h3>
+      <div className={s.section__wrap}>
+        <h2 className={`heading2 ${s.section__title}`}>
+          Volunteer application form
+        </h2>
+        <FormProvider {...methods}>
+          <form
+            id='form'
+            onSubmit={methods.handleSubmit(
+              onSubmit as SubmitHandler<FieldValues>,
+            )}
+            className={s.form}
+          >
+            <FullName nameType='firstName' />
+            <FullName nameType='lastName' />
+            <DateOfBirth />
+            <PhoneNumber />
+            <SelectCountry />
+            <SocialNetwork />
+            <Email />
+            <DaysOfVolunteering />
+            <Language />
+            <Skills />
+            <Comment />
+            <Checkboxes />
+            <ButtonApp
+              type='submit'
+              size='Xlarge'
+              className={s.form__button}
+              disabled={!isValid}
+            >
+              Send my form
+            </ButtonApp>
+          </form>
+        </FormProvider>
+      </div>
+    </section>
   );
 };
 
