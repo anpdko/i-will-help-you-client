93f7ceeb02306460937e8c765b4ac2265d61bc02
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-09-03 15:23:54 +0300

    fix: fix func

--- src/utils/generateSocialMediaLink.ts
+++ src/utils/generateSocialMediaLink.ts
@@ -2,15 +2,19 @@ export const generateSocialMediaLink = (
   network: string,
   networkLogo: string,
 ): string | null => {
+  const normalizedNetwork = network.startsWith('@')
+    ? network.slice(1)
+    : network;
+
   switch (networkLogo.toLowerCase()) {
     case 'telegram':
-      return `https://t.me/${network}`;
+      return `https://t.me/${normalizedNetwork}`;
     case 'viber':
-      return `viber://add?number=${network}`;
+      return `viber://add?number=${normalizedNetwork}`;
     case 'facebook':
-      return `https://www.facebook.com/${network}`;
+      return `https://www.facebook.com/${normalizedNetwork}`;
     case 'instagram':
-      return `https://www.instagram.com/${network}`;
+      return `https://www.instagram.com/${normalizedNetwork}`;
     default:
       return null;
   }
