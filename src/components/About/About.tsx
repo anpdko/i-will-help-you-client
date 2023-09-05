1ccff96eb4c31a3792bb77861fd54853c9e2d21e
Anastasia Koptilova <koptilova.nastya@gmail.com>
2023-09-05 20:46:20 +0300

    add about translation

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import aboutTab from '../../data/aboutTab';
 import AboutItem from './AboutItem/AboutItem';
 import sprite from '../../assets/sprite.svg';
@@ -6,6 +7,7 @@ import s from './About.module.scss';
 
 const About = () => {
   const [activeTab, setActiveTab] = useState('mission');
+  const { i18n } = useTranslation();
 
   const handleTabChange = (tab: string) => {
     setActiveTab(tab);
@@ -17,20 +19,27 @@ const About = () => {
         <h2 className='heading1'>Our Commitment and Vision</h2>
         <div className={s.about__body}>
           <div className={s.about__tabs}>
-            {aboutTab.map((tab) => (
-              <button
-                key={tab.id}
-                className={`${s.about__tab} ${
-                  activeTab === tab.id ? s.about__active : ''
-                }`}
-                onClick={() => handleTabChange(tab.id)}
-              >
-                <svg className={s.about__icon}>
-                  <use href={sprite + tab.tabIcon} />
-                </svg>
-                {tab.tabName}
-              </button>
-            ))}
+            {aboutTab.map((tab) => {
+              const transAbout = () => {
+                return tab.translations.find(
+                  (item) => i18n.language === item.language,
+                );
+              };
+              return (
+                <button
+                  key={tab.id}
+                  className={`${s.about__tab} ${
+                    activeTab === tab.id ? s.about__active : ''
+                  }`}
+                  onClick={() => handleTabChange(tab.id)}
+                >
+                  <svg className={s.about__icon}>
+                    <use href={sprite + tab.tabIcon} />
+                  </svg>
+                  {transAbout()?.tabName}
+                </button>
+              );
+            })}
           </div>
           <div className={s.about__items}>
             {aboutTab.map(
--- src/data/aboutTab.ts
+++ src/data/aboutTab.ts
@@ -1,70 +1,151 @@
+export interface ITranslation {
+  language: 'en' | 'ua';
+  description: string;
+  title: string;
+  tabName: string;
+}
+
 export interface ITabName {
   id: string;
-  tabName: string;
   tabIcon: string;
-  title: string;
   link: string;
-  description: string;
+  translations: ITranslation[];
 }
 
 const aboutTab: ITabName[] = [
   {
+    translations: [
+      {
+        language: 'en',
+        tabName: 'Mission',
+        title: 'Our Mission',
+        description:
+          "To stand as a guiding light for the millions of Ukrainians who sought refuge beyond their homeland's borders. We extend unwavering support, providing comprehensive assistance to navigate unfamiliar territories, and empowering individuals to rebuild with dignity and resilience.",
+      },
+      {
+        language: 'ua',
+        tabName: 'Місія',
+        title: 'Наша Місія',
+        description:
+          "To stand as a guiding light for the millions of Ukrainians who sought refuge beyond their homeland's borders. We extend unwavering support, providing comprehensive assistance to navigate unfamiliar territories, and empowering individuals to rebuild with dignity and resilience.",
+      },
+    ],
     id: 'mission',
-    tabName: 'Mission',
     tabIcon: '#mission',
-    title: 'Our Mission',
     link: '#',
-    description:
-      "To stand as a guiding light for the millions of Ukrainians who sought refuge beyond their homeland's borders. We extend unwavering support, providing comprehensive assistance to navigate unfamiliar territories, and empowering individuals to rebuild with dignity and resilience.",
   },
   {
+    translations: [
+      {
+        language: 'en',
+        tabName: 'Vision',
+        title: 'Our Vision',
+        description:
+          'A future where every Ukrainian refugee can find solace in the assurance of competent legal aid, where creative expression thrives, where education knows no boundaries, and where opportunities abound for professional growth and fulfillment.',
+      },
+      {
+        language: 'ua',
+        tabName: 'Бачення',
+        title: 'Наше Бачення',
+        description:
+          'A future where every Ukrainian refugee can find solace in the assurance of competent legal aid, where creative expression thrives, where education knows no boundaries, and where opportunities abound for professional growth and fulfillment.',
+      },
+    ],
     id: 'vision',
-    tabName: 'Vision',
     tabIcon: '#vision',
-    title: 'Our Vision',
     link: '#',
-    description:
-      'A future where every Ukrainian refugee can find solace in the assurance of competent legal aid, where creative expression thrives, where education knows no boundaries, and where opportunities abound for professional growth and fulfillment.',
   },
   {
-    id: 'objectives',
-    tabName: 'Key Objectives',
-    tabIcon: '#objectives',
-    title: 'Key Objectives',
-    link: '#',
-    description: `1. Holistic Legal Assistance: Providing legal guidance to navigate foreign systems.
+    translations: [
+      {
+        language: 'en',
+        tabName: 'Key Objectives',
+        title: 'Key Objectives',
+        description: `1. Holistic Legal Assistance: Providing legal guidance to navigate foreign systems.
       2. Creative Empowerment: Enriching global discourse through Ukrainian creativity.
       3. Educational Rebuilding: Bridging educational gaps and nurturing a generation of scholars.
       4. Empowering Professional Growth: Connecting Ukrainians to meaningful work opportunities.
       5. Psychological Healing: Extending support and resources to overcome trauma.
       6. Sustainable Change: Cultivating lasting partnerships and continuous refinement.`,
+      },
+      {
+        language: 'ua',
+        tabName: 'Ключові Цілі',
+        title: 'Ключові Цілі',
+        description: `1. Holistic Legal Assistance: Providing legal guidance to navigate foreign systems.
+      2. Creative Empowerment: Enriching global discourse through Ukrainian creativity.
+      3. Educational Rebuilding: Bridging educational gaps and nurturing a generation of scholars.
+      4. Empowering Professional Growth: Connecting Ukrainians to meaningful work opportunities.
+      5. Psychological Healing: Extending support and resources to overcome trauma.
+      6. Sustainable Change: Cultivating lasting partnerships and continuous refinement.`,
+      },
+    ],
+    id: 'objectives',
+    tabIcon: '#objectives',
+    link: '#',
   },
   {
+    translations: [
+      {
+        language: 'en',
+        tabName: 'Core Values',
+        title: 'Core Values',
+        description:
+          'Compassion, Empowerment, Inclusivity, Collaboration, and Resilience. Guided by these principles, we strive for excellence in all endeavors.',
+      },
+      {
+        language: 'ua',
+        tabName: 'Основні Цінності',
+        title: 'Основні Цінності',
+        description:
+          'Compassion, Empowerment, Inclusivity, Collaboration, and Resilience. Guided by these principles, we strive for excellence in all endeavors.',
+      },
+    ],
     id: 'values',
-    tabName: 'Core Values',
     tabIcon: '#values',
-    title: 'Core Values',
     link: '#',
-    description:
-      'Compassion, Empowerment, Inclusivity, Collaboration, and Resilience. Guided by these principles, we strive for excellence in all endeavors.',
   },
   {
+    translations: [
+      {
+        language: 'en',
+        tabName: 'Our Impact',
+        title: 'Our Impact',
+        description:
+          'Provide a summary of the impact the foundation has made so far. Include statistics, success stories, and achievements that demonstrate the positive outcomes of your projects and initiatives.',
+      },
+      {
+        language: 'ua',
+        tabName: 'Наш Вплив',
+        title: 'Наш Вплив',
+        description:
+          'Provide a summary of the impact the foundation has made so far. Include statistics, success stories, and achievements that demonstrate the positive outcomes of your projects and initiatives.',
+      },
+    ],
     id: 'impact',
-    tabName: 'Our Impact',
     tabIcon: '#impact',
-    title: 'Our Impact',
     link: '#',
-    description:
-      '[Provide a summary of the impact the foundation has made so far. Include statistics, success stories, and achievements that demonstrate the positive outcomes of your projects and initiatives.]',
   },
   {
+    translations: [
+      {
+        language: 'en',
+        tabName: 'Join Our Journey',
+        title: 'Make a Difference Today!',
+        description:
+          'Charity Foundation ”I Will Help You” is more than a foundation; it is a community of compassionate individuals who believe in the transformative power of healing. By joining our journey, you become part of a collective force working tirelessly to rebuild shattered lives and restore hope. Through your support, we can offer essential sustenance, education, mental health care, and meaningful opportunities to those who need it most. Together, we heal the wounds of war, mend broken dreams, and illuminate a path of resilience. Embark on this remarkable journey with us and witness the profound impact we can create together.',
+      },
+      {
+        language: 'ua',
+        tabName: 'Приєднуйся До Нас',
+        title: 'Зроби зміни сьогодні!',
+        description:
+          'Charity Foundation ”I Will Help You” is more than a foundation; it is a community of compassionate individuals who believe in the transformative power of healing. By joining our journey, you become part of a collective force working tirelessly to rebuild shattered lives and restore hope. Through your support, we can offer essential sustenance, education, mental health care, and meaningful opportunities to those who need it most. Together, we heal the wounds of war, mend broken dreams, and illuminate a path of resilience. Embark on this remarkable journey with us and witness the profound impact we can create together.',
+      },
+    ],
     id: 'journey',
-    tabName: 'Join Our Journey',
     tabIcon: '#journey',
-    title: 'Make a Difference Today!',
     link: '#',
-    description:
-      'Charity Foundation ”I Will Help You” is more than a foundation; it is a community of compassionate individuals who believe in the transformative power of healing. By joining our journey, you become part of a collective force working tirelessly to rebuild shattered lives and restore hope. Through your support, we can offer essential sustenance, education, mental health care, and meaningful opportunities to those who need it most. Together, we heal the wounds of war, mend broken dreams, and illuminate a path of resilience. Embark on this remarkable journey with us and witness the profound impact we can create together.',
   },
 ];
 
