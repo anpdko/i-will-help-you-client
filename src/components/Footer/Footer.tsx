f00d4663af0f84fde491ea7c8d751b1ee3f7dde9
DmytroKuts <40741910+Dmytro24@users.noreply.github.com>
2023-09-06 23:43:09 +0300

    fix mobile style .contacts

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import MediaQuery from 'react-responsive';
 
 import ButtonApp from '../UI/ButtonApp/ButtonApp';
 
@@ -63,7 +64,7 @@ const Footer = () => {
             </li>
             <li>
               <Link to='#' className={s.social__link}>
-                <svg >
+                <svg>
                   <use href={sprite + '#instagram'} />
                 </svg>
               </Link>
@@ -73,17 +74,48 @@ const Footer = () => {
 
         <div className={s.contacts}>
           <ButtonApp>{t('Donate')}</ButtonApp>
-          <ul className={s.contacts__list}>
-            <li>
-              <p>{t('Mon-Fri')} 8:00 - 18:00</p>
-            </li>
-            <li>
-              <Link to='tel:++380000000000'>+0 (000) 000 00 00</Link>
-            </li>
-            <li>
-              <Link to='mailto:iwillhelpu@post.com'>iwillhelpu@post.com</Link>
-            </li>
-          </ul>
+          <MediaQuery minWidth={391}>
+            <ul className={s.contacts__list}>
+              <li>
+                <p>{t('Mon-Fri')} 8:00 - 18:00</p>
+              </li>
+              <li>
+                <Link to='tel:++380000000000'>+0 (000) 000 00 00</Link>
+              </li>
+              <li>
+                <Link to='mailto:iwillhelpu@post.com'>iwillhelpu@post.com</Link>
+              </li>
+            </ul>
+          </MediaQuery>
+          <MediaQuery maxWidth={390}>
+            <ul className={s.contacts}>
+              <li className={s.contacts__item}>
+                <Link to='tel:++380000000000' className={s.contacts__link}>
+                  <div className={s.contacts__link_text}>
+                    <span>{t('Hotline')}</span>
+                    +0 (000) 000 00 00
+                  </div>
+                  <svg>
+                    <use href={sprite + '#phone'} />
+                  </svg>
+                </Link>
+              </li>
+              <li className={s.contacts__item}>
+                <Link
+                  to='mailto:iwillhelpu@post.com'
+                  className={s.contacts__link}
+                >
+                  <div className={s.contacts__link_text}>
+                    <span>{t('Email')}</span>
+                    iwillhelpu@post.com
+                  </div>
+                  <svg>
+                    <use href={sprite + '#mail'} />
+                  </svg>
+                </Link>
+              </li>
+            </ul>
+          </MediaQuery>
         </div>
       </div>
     </footer>
