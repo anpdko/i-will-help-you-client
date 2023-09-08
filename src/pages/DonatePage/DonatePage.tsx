532e1d30beefe51dffeb57d8ce04751faaac8a70
DmytroKuts <40741910+Dmytro24@users.noreply.github.com>
2023-09-08 14:15:51 +0300

    add slider ProjectsFund

--- src/pages/DonatePage/DonatePage.tsx
+++ src/pages/DonatePage/DonatePage.tsx
@@ -1,32 +1,10 @@
-import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
-
 import { FormHeader, ProjectsFund } from '../../components';
 
-import useFilteredProjects from '../../hooks/useFilteredProjects';
-
-import { Preloader } from '../../components/UI/';
-
-import { getProjects } from '../../store/projects/projectsSlice';
-import { RootState, useAppDispatch } from '../../store/store';
-
-import s from './DonatePage.module.scss';
-
 const DonatePage = () => {
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    dispatch(getProjects());
-  }, []);
-
-  const { loading } = useSelector((state: RootState) => state.projects);
-
-  const filteredProjects = useFilteredProjects();
-
   return (
     <>
       <FormHeader />
-      {loading ? <Preloader /> : <ProjectsFund projects={filteredProjects} />}
+      <ProjectsFund />
     </>
   );
 };
