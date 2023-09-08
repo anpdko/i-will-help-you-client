7257cbe7fbcf80667e8f511fe5c6a281d8ebcd42
DmytroKuts <40741910+Dmytro24@users.noreply.github.com>
2023-09-08 22:55:51 +0300

    create  AboutHeader, style and adaptive

--- src/components/AboutHeader/AboutHeader.tsx
+++ src/components/AboutHeader/AboutHeader.tsx
@@ -1,8 +1,39 @@
+import { useTranslation } from 'react-i18next';
+import MediaQuery from 'react-responsive';
+
+import FormingPng from '../../assets/image/forming-team-leadership-pana.png';
+
+import { ButtonApp } from '../UI';
+
+import s from './AboutHeader.module.scss';
 
 const AboutHeader = () => {
+  const { t } = useTranslation();
   return (
-    <section>
-      <div className='container'>AboutHeader</div>
+    <section className={s.about}>
+      <div className='container'>
+        <div className={s.content}>
+          <h1 className={`${s.content__title} heading1`}>{t('COMING SOON')}</h1>
+          <MediaQuery maxWidth={659}>
+            <div className={s.image}>
+              <img src={FormingPng} alt='Forming team leadership pana' />
+            </div>
+          </MediaQuery>
+          <p className={`${s.content__text} text`}>
+            {t(
+              'We are currently working hard on this page, please visit again later. You can become part of our team and help Ukrainians affected by the war',
+            )}
+          </p>
+          <ButtonApp type='link' to='/' size='small'>
+            {t('Back to Home')}
+          </ButtonApp>
+        </div>
+        <MediaQuery minWidth={660}>
+          <div className={s.image}>
+            <img src={FormingPng} alt='Forming team leadership pana' />
+          </div>
+        </MediaQuery>
+      </div>
     </section>
   );
 };
