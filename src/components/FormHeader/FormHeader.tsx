930eae341d3f40971ea42f5a48858a697ae6d599
DmytroKuts <40741910+Dmytro24@users.noreply.github.com>
2023-09-08 18:44:33 +0300

    fix FormWrapper line

--- src/components/FormHeader/FormHeader.tsx
+++ src/components/FormHeader/FormHeader.tsx
@@ -6,18 +6,20 @@ const FormHeader = () => {
   return (
     <section className={s.ready_need_header}>
       <div className='container'>
-        <div className={s.ready_need_header_text}>
-          <h1>Join our volunteer team</h1>
-          <p>
-            If you want to join our team of volunteers and help with what you do
-            best, fill out the form below and we are waiting for you.
-          </p>
-        </div>
-        <div className={s.ready_need_header_img}>
-          <img
-            src={API_URL + '/static/images/ready-need-header.jpg'}
-            alt='header image'
-          />
+        <div className={s.ready_need_header_body}>
+          <div className={s.ready_need_header_text}>
+            <h1>Join our volunteer team</h1>
+            <p>
+              If you want to join our team of volunteers and help with what you do
+              best, fill out the form below and we are waiting for you.
+            </p>
+          </div>
+          <div className={s.ready_need_header_img}>
+            <img
+              src={API_URL + '/static/images/ready-need-header.jpg'}
+              alt='header image'
+            />
+          </div>
         </div>
       </div>
     </section>
