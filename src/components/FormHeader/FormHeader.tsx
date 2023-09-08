00e11d133e3a19e825b2fd7cf42035b78d3dee47
DmytroKuts <40741910+Dmytro24@users.noreply.github.com>
2023-09-09 01:33:13 +0300

    refactoring FormHeader

--- src/components/FormHeader/FormHeader.tsx
+++ src/components/FormHeader/FormHeader.tsx
@@ -4,19 +4,19 @@ const API_URL = import.meta.env.VITE_API_URL;
 
 const FormHeader = () => {
   return (
-    <section className={s.ready_need_header}>
+    <section className={s.form_header}>
       <div className='container'>
-        <div className={s.ready_need_header_body}>
-          <div className={s.ready_need_header_text}>
-            <h1>Join our volunteer team</h1>
-            <p>
+        <div className={s.body}>
+          <div className={s.body__content}>
+            <h1 className='heading1'>Join our volunteer team</h1>
+            <p className='text'>
               If you want to join our team of volunteers and help with what you
               do best, fill out the form below and we are waiting for you.
             </p>
           </div>
-          <div className={s.ready_need_header_img}>
+          <div className={s.body__image}>
             <img
-              src={API_URL + '/static/images/ready-need-header.jpg'}
+              src={API_URL + '/static/images/banners/banner-1.jpg'}
               alt='header image'
             />
           </div>
