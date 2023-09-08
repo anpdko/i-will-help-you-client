aefc6ce50c312747ff44203128ef0eb83762e95c
DmytroKuts <40741910+Dmytro24@users.noreply.github.com>
2023-09-09 00:34:19 +0300

    add redirect for 404 not found

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,6 +1,27 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { NotFoundBlock } from '../../components';
 
 const NotFoundPage = () => {
+  const [redirect, setRedirect] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setRedirect(true);
+    }, 5000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (redirect) {
+      navigate('/');
+    }
+  }, [redirect]);
+
   return (
     <>
       <NotFoundBlock />
