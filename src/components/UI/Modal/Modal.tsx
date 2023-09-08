a5b8e0649d9cfc17cb99df2a7514617b8a282aa4
Olesia Andreiko <olesikmail@gmail.com>
2023-09-08 19:29:42 +0300

    add modal component

---
+++ src/components/UI/Modal/Modal.tsx
@@ -0,0 +1,57 @@
+import React, { useEffect } from 'react';
+import s from './Modal.module.scss';
+import sprite from '../../../assets/sprite.svg';
+
+interface ModalProps {
+  children: React.ReactNode;
+  onClose: () => void;
+  className?: string;
+  size?: 'smaller' | 'small' | 'large';
+}
+
+const Modal = ({ children, className, size = 'smaller', onClose }: ModalProps) => {
+  const onBackdropClose = (event:React.MouseEvent) => {
+    if (event.target === event.currentTarget) {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    const onEscapeClose = (event:React.KeyboardEvent) => {
+      if (event.code === 'Escape') {
+        onClose();
+      }
+    };
+
+    window.addEventListener('keydown', onEscapeClose);
+    document.body.style.overflow = 'hidden';
+
+    return () => {
+      document.body.style.overflow = 'visible';
+      window.removeEventListener('keydown', onEscapeClose);
+    };
+  }, [onClose]);
+
+  return (
+    <>
+      <div className={s.overlay} onClick={onBackdropClose}>
+        <div className={'container'}>
+          <div className={[s.overlay__modal, s[size], className].join(' ')}>
+            <button
+              type='button'
+              className={s.overlay__modal_button}
+              onClick={onClose}
+            >
+              <svg>
+                <use href={sprite + '#cross'} />
+              </svg>
+            </button>
+            <div className={s.overlay__modal_content}>{children}</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Modal;
--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -1,9 +1,29 @@
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import ButtonApp from '../../components/UI/ButtonApp/ButtonApp';
+import Modal from '../../components/UI/Modal/Modal';
+import ModalSuccess from '../../components/ModalSuccess/ModalSuccess';
+
 const AboutPage = () => {
+  const { t } = useTranslation();
+
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
+  const toggleModal = () => {
+    setIsOpenModal((isOpenModal) => !isOpenModal);
+  };
+
   return (
     <>
       <section>
         <div className='container'>
           <h1>About</h1>
+          <ButtonApp onClick={toggleModal}>{t('Button Modal')}</ButtonApp>
+          {isOpenModal && (
+            <Modal onClose={toggleModal}>
+              <ModalSuccess onClose={toggleModal}/>
+            </Modal>
+          )}
         </div>
       </section>
     </>
