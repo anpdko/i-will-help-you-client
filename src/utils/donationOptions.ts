077315b8b41044f8422d08f05fa2b954e63fff24
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-09-09 23:10:21 +0300

    feat: add donation options for payment

---
+++ src/utils/donationOptions.ts
@@ -0,0 +1,52 @@
+export const paymentMethod = [
+  {
+    id: 'card',
+    title: 'Card',
+  },
+  {
+    id: 'transfer',
+    title: 'Transfer',
+  },
+  {
+    id: 'crypto',
+    title: 'Crypto',
+  },
+];
+
+export const paymentFrequency = [
+  {
+    id: 'oneTime',
+    title: 'One time payment',
+  },
+  {
+    id: 'subscription',
+    title: 'Subscription',
+  },
+];
+
+export const donationAmount = [
+  {
+    id: '1',
+    title: '1$',
+  },
+  {
+    id: '3',
+    title: '3$',
+  },
+  {
+    id: '10',
+    title: '10$',
+  },
+  {
+    id: '25',
+    title: '25$',
+  },
+  {
+    id: '100',
+    title: '100$',
+  },
+  {
+    id: '500',
+    title: '500$',
+  },
+];
