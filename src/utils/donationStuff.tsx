3b2d751afccbf301103abb1e4a3eaa35736566e3
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-09-09 21:04:07 +0300

    feat: add donation stuff list

---
+++ src/utils/donationStuff.tsx
@@ -0,0 +1,102 @@
+export const donateStuffList = [
+  {
+    title: 'Clothing',
+    desc: 'Gently used or new clothing items for various age groups.',
+    icon: 'src/assets/icons/donation/clothing.svg',
+  },
+  {
+    title: 'Non-perishable Food',
+    desc: 'Canned goods, dry grains, pasta, and other non-perishable food items.',
+    icon: 'src/assets/icons/donation/non-perishable-food.svg',
+  },
+  {
+    title: 'Toiletries',
+    desc: 'Hygiene products like toothbrushes, toothpaste, soap, shampoo, and sanitary products.',
+    icon: 'src/assets/icons/donation/toiletries.svg',
+  },
+  {
+    title: 'Bedding and Linens',
+    desc: 'Blankets, sheets, towels, and pillows in good condition.',
+    icon: 'src/assets/icons/donation/bedding-and-linens.svg',
+  },
+  {
+    title: 'School Supplies',
+    desc: 'Notebooks, pens, pencils, backpacks, and other school-related items.',
+    icon: 'src/assets/icons/donation/school-supplies.svg',
+  },
+  {
+    title: 'Baby Supplies',
+    desc: 'Diapers, formula, baby food, baby clothes, and baby care products.',
+    icon: 'src/assets/icons/donation/baby-supplies.svg',
+  },
+  {
+    title: 'Medical Supplies',
+    desc: 'Over-the-counter medications, bandages, and basic medical supplies.',
+    icon: 'src/assets/icons/donation/medical-supplies.svg',
+  },
+  {
+    title: 'Educational Materials',
+    desc: 'Books, educational toys, and learning resources for all ages.',
+    icon: 'src/assets/icons/donation/educational-materials.svg',
+  },
+  {
+    title: 'Winter Wear',
+    desc: 'Warm clothing, jackets, gloves, hats, and scarves for colder months.',
+    icon: 'src/assets/icons/donation/winter-wear.svg',
+  },
+  {
+    title: 'Footwear',
+    desc: 'Shoes and socks for adults and children.',
+    icon: 'src/assets/icons/donation/footwear.svg',
+  },
+  {
+    title: 'Household Essentials',
+    desc: 'Cookware, utensils, small appliances, and cleaning supplies.',
+    icon: 'src/assets/icons/donation/household-essentials.svg',
+  },
+  {
+    title: 'Technology',
+    desc: 'Functional laptops, tablets, and smartphones for educational purposes.',
+    icon: 'src/assets/icons/donation/technology.svg',
+  },
+  {
+    title: 'Baby Gear',
+    desc: 'Strollers, cribs, playpens, and other baby equipment in good condition.',
+    icon: 'src/assets/icons/donation/baby-gear.svg',
+  },
+  {
+    title: 'Sports Equipment',
+    desc: 'Balls, bicycles, and sports gear for recreational activities.',
+    icon: 'src/assets/icons/donation/sports-equipment.svg',
+  },
+  {
+    title: 'Furniture',
+    desc: 'Basic furniture items like chairs, tables, and shelves in usable condition.',
+    icon: 'src/assets/icons/donation/furniture.svg',
+  },
+  {
+    title: 'Art Supplies',
+    desc: 'Craft materials, art supplies, and coloring books for creative expression.',
+    icon: 'src/assets/icons/donation/art-supplies.svg',
+  },
+  {
+    title: 'Electronics',
+    desc: 'Functional electronics like radios, fans, and small appliances.',
+    icon: 'src/assets/icons/donation/electronics.svg',
+  },
+  {
+    title: 'Pet Supplies',
+    desc: 'Pet food, pet beds, and basic pet care items for families with animals.',
+    icon: 'src/assets/icons/donation/pet-supplies.svg',
+  },
+  {
+    title: 'Kitchen Utensils',
+    desc: 'Plates, cups, utensils, and cooking tools for families.',
+    icon: 'src/assets/icons/donation/kitchen-utensils.svg',
+  },
+  {
+    title: 'Hygiene Kits',
+    desc: 'Assembled kits with essentials like soap, toothbrushes, and shampoo.',
+    icon: 'src/assets/icons/donation/hygiene-kits.svg',
+  },
+];
