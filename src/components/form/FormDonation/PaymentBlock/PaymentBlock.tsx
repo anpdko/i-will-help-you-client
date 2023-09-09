926f8f54a91a308e3d9a5174cf925d4ef715ecd1
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-09-09 23:20:09 +0300

    feat: add payment block to form

---
+++ src/components/form/FormDonation/PaymentBlock/PaymentBlock.tsx
@@ -0,0 +1,121 @@
+import { ChangeEvent, useEffect, useState } from 'react';
+import { Controller, useFormContext } from 'react-hook-form';
+import {
+  paymentMethod,
+  paymentFrequency,
+  donationAmount,
+} from '../../../../utils/donationOptions';
+import RadioInput from '../RadioInput/RadioInput';
+import s from './PaymentBlock.module.scss';
+
+const PaymentBlock = () => {
+  const { control, setValue, getValues } = useFormContext();
+  const [customAmount, setCustomAmount] = useState('');
+
+  const handleCustomInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setCustomAmount(e.target.value);
+    setValue('donationAmount', e.target.value);
+  };
+
+  useEffect(() => {
+    const selectedValue = getValues('donationAmount');
+    if (!selectedValue) {
+      setValue('donationAmount', '10');
+    }
+  }, []);
+
+  return (
+    <div className={s.paymentBlock}>
+      <div className={`border-style ${s.paymentBlock__method}`}>
+        {paymentMethod.map((item) => (
+          <Controller
+            key={item.id}
+            name='paymentMethod'
+            rules={{ required: true }}
+            control={control}
+            defaultValue={item.id === 'card' ? item.id : null}
+            render={({ field }) => (
+              <>
+                <RadioInput
+                  name={field.name}
+                  id={item.id}
+                  onChange={field.onChange}
+                  value={field.value}
+                  title={item.title}
+                />
+                <span className={s.decorativeLine}></span>
+              </>
+            )}
+          />
+        ))}
+      </div>
+      <div className={`border-style ${s.paymentBlock__frequency}`}>
+        {paymentFrequency.map((item) => (
+          <Controller
+            key={item.id}
+            name='paymentFrequency'
+            rules={{ required: true }}
+            control={control}
+            defaultValue={item.id === 'oneTime' ? item.id : null}
+            render={({ field }) => (
+              <>
+                <RadioInput
+                  name={field.name}
+                  id={item.id}
+                  onChange={field.onChange}
+                  value={field.value}
+                  title={item.title}
+                />
+                <span className={s.decorativeLine}></span>
+              </>
+            )}
+          />
+        ))}
+      </div>
+      <div className={s.paymentBlock__amount}>
+        <div className={`border-style ${s.paymentBlock__amount_number}`}>
+          {donationAmount.map((item) => (
+            <Controller
+              key={item.id}
+              name='donationAmount'
+              rules={{ required: true }}
+              control={control}
+              defaultValue={item.id === '10' ? item.id : null}
+              render={({ field }) => (
+                <>
+                  <RadioInput
+                    name={field.name}
+                    id={item.id}
+                    value={field.value}
+                    title={item.title}
+                    onChange={(e) => {
+                      field.onChange(e);
+                      setCustomAmount('');
+                    }}
+                    checked={getValues('donationAmount') === item.id}
+                  />
+                  <span className={s.decorativeLine}></span>
+                </>
+              )}
+            />
+          ))}
+        </div>
+        <label
+          htmlFor='customDonationAmount'
+          className={`border-style ${s.paymentBlock__amount_custom}`}
+        >
+          <input
+            type='text'
+            name='customDonationAmount'
+            id='customDonationAmount'
+            placeholder='$ your donation amount'
+            value={customAmount}
+            onChange={handleCustomInputChange}
+          />
+        </label>
+      </div>
+    </div>
+  );
+};
+
+export default PaymentBlock;
