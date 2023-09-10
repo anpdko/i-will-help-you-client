b7a6039e0f5581997c315fa3af6107ec010a06b2
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-09-10 15:04:53 +0300

    feat: add alias to project

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), ViteImageOptimizer()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@store': path.resolve(__dirname, './src/store'),
+    },
+  },
 });
