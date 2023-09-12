145cc9b0c545e7fe51bf2ffc9a0b8500f466b907
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-09-12 18:03:46 +0300

    fix: change Comment components for different title and placeholder

--- src/components/form/Comment/Comment.tsx
+++ src/components/form/Comment/Comment.tsx
@@ -3,19 +3,21 @@ import FormItemWrapper from '../FormItemWrapper/FormItemWrapper';
 import { TextareaFormApp } from '../../UI';
 import s from './Comment.module.scss';
 
-const Comment = () => {
+interface CommentProps {
+  title: string;
+  placeholder: string;
+}
+
+const Comment = ({ title, placeholder }: CommentProps) => {
   const { t } = useTranslation();
 
   return (
-    <FormItemWrapper
-      className={s.comment}
-      title={t('Comment (what do you to do?; in what areas?)')}
-    >
+    <FormItemWrapper className={s.comment} title={t(title)}>
       <TextareaFormApp
         name='comment'
         id='comment'
         rows={10}
-        placeholder={t('Type here...')}
+        placeholder={t(placeholder)}
       />
     </FormItemWrapper>
   );
