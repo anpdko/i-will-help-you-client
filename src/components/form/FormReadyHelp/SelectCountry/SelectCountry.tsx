7cc74560e847d2d4e5686fd715d528a81b0755af
Anastasia Koptilova <koptilova.nastya@gmail.com>
2023-09-12 21:18:58 +0300

    add countries translate

--- src/components/form/FormReadyHelp/SelectCountry/SelectCountry.tsx
+++ src/components/form/FormReadyHelp/SelectCountry/SelectCountry.tsx
@@ -1,13 +1,30 @@
+import { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import { countryList } from '../../../../utils/listOfCountry';
+import { countryList, countryListUa } from '../../../../utils/listOfCountry';
 import { SelectInput } from '../../../UI';
 import s from './SelectCountry.module.scss';
 import FormItemWrapper from '../../FormItemWrapper/FormItemWrapper';
 
 const SelectCountry = () => {
+  const value = localStorage.getItem('language');
+  const [lang, setLang] = useState('en');
+
+  useEffect(() => {
+    if (value) {
+      setLang(JSON.parse(value));
+    } else {
+      return;
+    }
+  }, [value]);
+
   const { t } = useTranslation();
 
   const countries = countryList.map((country) => ({
+    value: country.name,
+    label: country.name,
+  }));
+
+  const countriesUa = countryListUa.map((country) => ({
     value: country.name,
     label: country.name,
   }));
@@ -16,7 +33,7 @@ const SelectCountry = () => {
     <FormItemWrapper className={s.country} title={t('Country of Residence *')}>
       <SelectInput
         name='country'
-        options={countries}
+        options={lang === 'ua' ? countriesUa : countries}
         placeholder={t('Select Country')}
         required={true}
         className={s.country__input}
