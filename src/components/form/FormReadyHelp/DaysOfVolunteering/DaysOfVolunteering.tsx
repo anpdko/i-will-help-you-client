153c4035176ed124cd8d14181c1b102a4a67064d
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-09-14 13:05:02 +0300

    feat: add new icons from react-icons library

--- src/components/form/FormReadyHelp/DaysOfVolunteering/DaysOfVolunteering.tsx
+++ src/components/form/FormReadyHelp/DaysOfVolunteering/DaysOfVolunteering.tsx
@@ -4,8 +4,7 @@ import Select from 'react-select';
 import customStyles from '@components/UI/form/SelectInput/selectStyle';
 import { daysOfWeekOptions, timeOptions } from '@utils/daysOfVolunteeringList';
 import FormItemWrapper from '../../FormItemWrapper/FormItemWrapper';
-import { PlusIcon } from '@components/icons/PlusIcon';
-import { DeleteIcon } from '@components/icons/DeleteIcon';
+import { HiOutlinePlus, HiOutlineX } from 'react-icons/hi';
 import s from './DaysOfVolunteering.module.scss';
 
 const DaysOfVolunteering = () => {
@@ -128,7 +127,7 @@ const DaysOfVolunteering = () => {
               onClick={() => remove(index)}
               className={s.volunteer__button_remove}
             >
-              <DeleteIcon />
+              <HiOutlineX />
             </button>
           )}
         </div>
@@ -138,7 +137,7 @@ const DaysOfVolunteering = () => {
         onClick={addDaysField}
         className={s.volunteer__button_add}
       >
-        {t('Add one more')} <PlusIcon />
+        {t('Add one more')} <HiOutlinePlus />
       </button>
     </FormItemWrapper>
   );
