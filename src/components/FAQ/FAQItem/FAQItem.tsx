c6d818966ad0a0bb7785815fc7bb13537f7c6624
DmytroKuts <40741910+Dmytro24@users.noreply.github.com>
2023-09-14 15:53:09 +0300

    npm run format

--- src/components/FAQ/FAQItem/FAQItem.tsx
+++ src/components/FAQ/FAQItem/FAQItem.tsx
@@ -21,9 +21,7 @@ const FAQItem = ({ tab }: FAQProps) => {
         <button type='button' className={s.item__btn}>
           <svg
             className={
-              toggleFAQ
-                ? `${s.item__icon_open}`
-                : `${s.item__icon_closed}`
+              toggleFAQ ? `${s.item__icon_open}` : `${s.item__icon_closed}`
             }
           >
             <use href={sprite + '#chevron-down'} />
@@ -32,9 +30,7 @@ const FAQItem = ({ tab }: FAQProps) => {
       </div>
       <div
         className={
-          toggleFAQ
-            ? `${s.item__content_open}`
-            : `${s.item__content_closed}`
+          toggleFAQ ? `${s.item__content_open}` : `${s.item__content_closed}`
         }
       >
         <div className={s.item__descr}>
