715be45543d4f425ef39ccbffa734b756ea300e3
DmytroKuts <40741910+Dmytro24@users.noreply.github.com>
2023-09-16 17:18:17 +0300

    fix mobile review slider padding  hidden

--- src/components/Reviews/Reviews.tsx
+++ src/components/Reviews/Reviews.tsx
@@ -3,12 +3,14 @@ import { useTranslation } from 'react-i18next';
 import ReviewsDesktop from './ReviewsDesktop/ReviewsDesktop';
 import ReviewsMobile from './ReviewsMobile/ReviewsMobile';
 
+import s from './Reviews.module.scss';
+
 const Reviews = () => {
   const { t } = useTranslation();
   const isMobile = useMediaQuery({ maxWidth: 991 });
 
   return (
-    <section>
+    <section className={s.reviews}>
       <div className='container'>
         <h2 className='heading1'>{t('Stories of Hope and Gratitude')}</h2>
         {isMobile ? <ReviewsMobile /> : <ReviewsDesktop />}
