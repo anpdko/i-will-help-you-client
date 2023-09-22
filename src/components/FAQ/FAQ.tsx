c6b5129fe9f3039883e6f6f310019400ed8a9e76
DmytroKuts <40741910+Dmytro24@users.noreply.github.com>
2023-09-22 04:06:49 +0300

    added search for FAQ

--- src/components/FAQ/FAQ.tsx
+++ src/components/FAQ/FAQ.tsx
@@ -1,9 +1,10 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { IFaqData, ITranslation } from '../../data/faqData';
 import FAQItem from './FAQItem/FAQItem';
 
+import sprite from '../../assets/sprite.svg';
 import s from './FAQ.module.scss';
 
 interface FAQProps {
@@ -12,8 +13,10 @@ interface FAQProps {
 
 const FAQ = ({ data }: FAQProps) => {
   const { t, i18n } = useTranslation();
-  const [visibleFAQCount, setVisibleFAQCount] = useState(4); // Початкова кількість видимих пунктів FAQ
-
+  const [visibleFAQCount, setVisibleFAQCount] = useState(4);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchResults, setSearchResults] = useState<IFaqData[]>([]);
+  const [isSearchActive, setIsSearchActive] = useState(false);
   const getTranslation = (faq: IFaqData): ITranslation | undefined => {
     return faq.translations.find(
       (translation) => translation.language === i18n.language,
@@ -24,21 +27,64 @@ const FAQ = ({ data }: FAQProps) => {
     setVisibleFAQCount(data.length);
   };
 
+  const handleSearchButtonClick = () => {
+    setIsSearchActive(!isSearchActive);
+  };
+
+  const handleSearchInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    const query = event.target.value;
+    setSearchQuery(query);
+    if (query) {
+      const results = data.filter((faq) => {
+        const translation = getTranslation(faq);
+        return (
+          translation &&
+          translation.title.toLowerCase().includes(query.toLowerCase())
+        );
+      });
+      setSearchResults(results);
+    } else {
+      setSearchResults([]);
+    }
+  };
+
   return (
     <section className={s.faq}>
       <div className='container'>
         <div className={s.header}>
-          <h2 className='heading1'>FAQ</h2>
+          <div className={s.column}>
+            <h2 className='heading1'>FAQ</h2>
+            <div className={s.search}>
+              <input
+                className={`${s.input} ${isSearchActive ? `${s.show}` : ''}`}
+                type='text'
+                value={searchQuery}
+                onChange={handleSearchInputChange}
+              />
+              <button aria-label='Search' onClick={handleSearchButtonClick}>
+                <svg>
+                  <use href={sprite + '#search'} />
+                </svg>
+              </button>
+            </div>
+          </div>
+
           <h3 className={s.subtitle}>FAQ</h3>
         </div>
         <div className={s.body}>
           <ul>
-            {data.slice(0, visibleFAQCount).map((faq) => (
-              <FAQItem key={faq._id} translation={getTranslation(faq)} />
-            ))}
+            {(searchQuery ? searchResults : data.slice(0, visibleFAQCount)).map(
+              (faq) => (
+                <FAQItem key={faq._id} translation={getTranslation(faq)} />
+              ),
+            )}
           </ul>
-          {visibleFAQCount < data.length && (
-            <button className={s.more} onClick={handleOpenAllFAQ}>{t('Open more FAQ')}</button>
+          {visibleFAQCount < data.length && !searchQuery && (
+            <button className={s.more} onClick={handleOpenAllFAQ}>
+              {t('Open more FAQ')}
+            </button>
           )}
         </div>
       </div>
