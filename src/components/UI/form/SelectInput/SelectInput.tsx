5ab891c4ca93ee4a7a8c48644b7be15114a28955
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-09-25 15:08:04 +0300

    fix: fix Select input

--- src/components/UI/form/SelectInput/SelectInput.tsx
+++ src/components/UI/form/SelectInput/SelectInput.tsx
@@ -1,58 +1,60 @@
-import { Controller, useFormContext } from 'react-hook-form';
-import { Select } from 'antd';
-import s from './SelectInput.module.scss';
+/* eslint-disable prettier/prettier */
+import { ConfigProvider, Select, SelectProps } from 'antd';
 import './SelectStyles.scss';
 
 interface SelectInputProps {
-  name: string;
-  commonName?: string;
-  index?: number;
-  options: any;
+  field: {
+    onChange: (value: string) => void;
+    onBlur: () => void;
+    value: string;
+    ref: React.Ref<any>;
+  };
+  options: SelectProps<any>['options'];
   placeholder: string;
-  isMulti?: boolean;
   isSearchable?: boolean;
-  required?: boolean;
-  errorMessage?: string;
   className?: string;
 }
 
 const SelectInput = ({
-  commonName,
-  name,
-  index,
+  field,
   options,
   placeholder,
-  isSearchable = true,
-  required = false,
-  errorMessage,
+  isSearchable = false,
   className,
 }: SelectInputProps) => {
-  const {
-    control,
-    formState: { errors },
-  } = useFormContext();
-
   return (
-    <Controller
-      name={index ? `${commonName}[${index}].${name}` : name}
-      control={control}
-      defaultValue={null}
-      rules={{ required: required }}
-      render={({ field }) => (
-        <div className={s.wrap}>
-          <Select
-            {...field}
-            options={options}
-            placeholder={placeholder}
-            showSearch={isSearchable}
-            className={`${className}`}
-          />
-          {required && errorMessage && errors.country && (
-            <p className={s.error}>{errorMessage}</p>
-          )}
-        </div>
-      )}
-    />
+    <ConfigProvider
+      theme={{
+        components: {
+          Select: {
+            multipleItemHeight: 34,
+            optionFontSize: 14,
+            optionSelectedBg: '#EEE',
+            optionPadding: '15px 24px',
+          },
+        },
+        token: {
+          controlHeight: 52,
+          colorBorder: '#000',
+          colorPrimaryHover: '#000',
+          borderRadius: 10,
+          fontFamily: 'Open Sans',
+          fontSize: 16,
+          colorTextPlaceholder: '#505050',
+        },
+      }}
+    >
+      <Select
+        onChange={field.onChange}
+        onBlur={field.onBlur}
+        value={field.value}
+        ref={field.ref}
+        options={options}
+        placeholder={placeholder}
+        showSearch={isSearchable}
+        className={className}
+      />
+    </ConfigProvider>
   );
 };
 
